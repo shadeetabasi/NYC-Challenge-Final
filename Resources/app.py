f78bf6acc6df75d7bbb51c2fb339d87edfeb4056
Shadee Tabasi <shadeetabasi@gmail.com>
2021-04-30 20:42:39 -0400

    front end updates

--- Resources/app.py
+++ Resources/app.py
@@ -6,9 +6,10 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify, render_template, redirect, Response
+from flask import Flask, jsonify, render_template, redirect, Response, request
 import json
 from pprint import pprint
+from copy import deepcopy
 
 # Flask Setup
 app = Flask(__name__)
@@ -17,6 +18,10 @@ app.static_folder = 'static'
 #Create connection to sql database
 connection_string = "postgres:postgres@localhost:5432/final_project"
 engine = create_engine(f'postgresql://{connection_string}')
+
+geojson_path = "/Users/shadeetabasi/code/NYC-Challenge-Final/Resources/static/data/nyc_zipcodes.geojson"
+with open(geojson_path, 'r') as f:
+    nyc_zipcodes = json.load(f)
 
 #################################################
 # Flask Routes
@@ -35,32 +40,40 @@ def loader():
     
     return render_template("loader.html")
 
-@app.route("/dashboard")
+@app.route("/dashboard", methods=['GET','POST'])
 def dashboard():
+
+    # Query the user's input fields ... and set defaults to display
+    zip_code = request.form.get("zip_code", 10010)
+    property_type = request.form.get("property_type", "condo")
+    bedrooms = request.form.get("bedrooms", 2)
+    baths = request.form.get("baths", 2)
+    print("Request", dict(request.form))
+
     # 
-    map_data = []
-    query = """SELECT lat, long, mls_property_type, borough
-                FROM public.real_estate"""
+    map_data = deepcopy(nyc_zipcodes)
+    query = f"""select avg(sold_price) as price, zipcode from real_estate 
+                where bed = {bedrooms} and bath = {baths} 
+                group by zipcode
+            """
+
     # Open a connection to SQL
     connection = engine.connect()
-    # Execute query
-    result = engine.execute(query)
-    # Append each result to our list
-    for row in result:
-        map_data.append({
-            "lat": row[0],
-            "long": row[1],
-            "mls_property_type": row[2],
-            "borough": row[3]
-        })
+    # Execute query and collect price per zipcode
+    price_per_zip = {str(row[1]): row[0] for row in engine.execute(query)}
     # Close the connection
     connection.close()
+
+    for feature in map_data["features"]:
+        zipcode = feature["properties"]["postalCode"]
+        price = price_per_zip.get(zipcode, 0)
+        feature["properties"]["avg_price"] = price
 
     # Return template and data
     # Note: follow this example for how to pass into a map
     # https://stackoverflow.com/questions/42499535/passing-a-json-object-from-flask-to-javascript
-    return render_template("dashboard2.html", map_data=map_data)
+    return render_template("dashboard2.html", map_data=map_data, zip_code=zip_code, baths=baths, bedrooms=bedrooms, property_type = property_type)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- Resources/static/js/logic2.js
+++ Resources/static/js/logic2.js
@@ -1,97 +1,48 @@
-// Creating our initial map object.
-// We set the longitude, latitude, and the starting zoom level for sf.
-// This gets inserted into the div with an id of 'map' in index.html.
+
+// Creating map object
 var myMap = L.map("leafletmap", {
-    center: [40.7128, -74.0060],
-    zoom: 5,
-  });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  // Store our API endpoint
-// var queryUrl = "https://tree-map.nycgovparks.org/";
-//  GET color radius call to the query URL
-// d3.json(queryUrl, function(data) {
-//   function styleInfo(feature) {
-//     return {
-//       opacity: 1,
-//       fillOpacity: 1,
-//       fillColor: getColor(feature.properties.mag),
-//       color: "#000000",
-//       radius: getRadius(feature.properties.mag),
-//       stroke: true,
-//       weight: 0.5
-//     };
-//   }
-//   // set different color from magnitude
-//     function getColor(magnitude) {
-//     switch (true) {
-//     case magnitude > 5:
-//       return "#EA2C2C";
-//     case magnitude > 4:
-//       return "#EA822C";
-//     case magnitude > 3:
-//       return "#EE9C00";
-//     case magnitude > 2:
-//       return "#EECC00";
-//     case magnitude > 1:
-//       return "#D4EE00";
-//     default:
-//       return "#98EE00";
-//     }
-//   }
-//   // set radiuss from magnitude
-//     function getRadius(magnitude) {
-//     if (magnitude === 0) {
-//       return 1;
-//     }
-//     return magnitude * 4;
-//   }
-//     // GeoJSON layer
-//     L.geoJson(data, {
-//       // Maken cricles
-//       pointToLayer: function(feature, latlng) {
-//         return L.circleMarker(latlng);
-//       },
-//       // cirecle style
-//       style: styleInfo,
-//       // popup for each marker
-//       onEachFeature: function(feature, layer) {
-//         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-//       }
-//     }).addTo(myMap);
-//     // an object legend
-//     var legend = L.control({
-//       position: "bottomright"
-//     });
-//     // details for the legend
-//     legend.onAdd = function() {
-//       var div = L.DomUtil.create("div", "info legend");
-//       var grades = [0, 1, 2, 3, 4, 5];
-//       var colors = [
-//         "#98EE00",
-//         "#D4EE00",
-//         "#EECC00",
-//         "#EE9C00",
-//         "#EA822C",
-//         "#EA2C2C"
-//       ];
-//       // Looping through
-//       for (var i = 0; i < grades.length; i++) {
-//         div.innerHTML +=
-//           "<i style='background: " + colors[i] + "'></i> " +
-//           grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-//       }
-//       return div;
-//     };
-//     // Finally, we our legend to the map.
-//     legend.addTo(myMap);
-//   });
+  center: [40.7128, -74.0059],
+  zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+// Grabbing our GeoJSON data..
+// d3.json(link, function(data) {
+//   // Creating a GeoJSON layer with the retrieved data
+//   L.geoJson(nyc_map_data).addTo(myMap);
+// });
+
+function getColor(d) {
+  return d > 2000000 ? '#800026' :
+         d > 1000000  ? '#BD0026' :
+         d > 800000  ? '#E31A1C' :
+         d > 600000  ? '#FC4E2A' :
+         d > 500000   ? '#FD8D3C' :
+         d > 200000   ? '#FEB24C' :
+         d > 100000   ? '#FED976' :
+                    '#FFEDA0';
+}
+
+function style(feature) {
+  return {
+      fillColor: getColor(feature.properties.avg_price),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+  };
+}
+
+
+L.geoJson(map_data, {style: style}).addTo(myMap);
